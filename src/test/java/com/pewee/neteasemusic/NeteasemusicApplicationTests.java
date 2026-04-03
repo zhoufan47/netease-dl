068bf9e151fec3889d550b3048dea7d870d17ba4
yzw <690450725@qq.com>
2026-04-03 13:57:17 +0800

    delete tests

--- src/test/java/com/pewee/neteasemusic/NeteasemusicApplicationTests.java
+++ src/test/java/com/pewee/neteasemusic/NeteasemusicApplicationTests.java
@@ -8,16 +8,5 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class NeteasemusicApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
-	
-	public static void main(String[] args) {
-		Long mills = 1749657600000L;
-		Date date = new Date(mills);
-		
-		System.out.println(date);
-		
-	}
 
 }
